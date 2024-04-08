986b888059348e016d1d45f088c01dd21ca91e11
Luis Jeffrey Rojas Montes <jeffreyrm96@gmail.com>
2024-04-08 16:14:29 -0500

    fix(home): main view selector

--- app.js
+++ app.js
@@ -20,13 +20,22 @@ document.addEventListener("DOMContentLoaded", () => {
     let mainContainer = document.getElementById("main_container");
     mainContainer.appendChild(test);
     to_do.forEach((element, index) => {
-        let cbh = GI.createElement("div");
-        cbh.appendChild(document.createTextNode("prueba"));
+        let parent = GI.createElement("div");
         let cbx = GI.createElement("input");
         cbx.type = "checkbox";
         cbx.name = "cbx" + index;
         cbx.value = element;
-        cbx.appendChild(cbh);
-        mainContainer.appendChild(cbx);
+        parent.appendChild(cbx);
+        parent.appendChild(document.createTextNode(element));
+        cbx.addEventListener("change", (value) => {
+            if(value.target.checked) {
+                parent.style["text-decoration"] = "line-through";
+                parent.style["color"] = "gray";
+            } else {
+                parent.style["text-decoration"] = "none";
+                parent.style["color"] = "black";
+            }
+        });
+        mainContainer.appendChild(parent);
     });
 });
