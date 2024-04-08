418828d68ef87aa192e95b382a1746c5c5b930ec
Luis Jeffrey Rojas Montes <jeffreyrm96@gmail.com>
2024-04-08 16:03:07 -0500

    fix(home): main view selector

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ const to_do = [
     'Comprar entradas para el cine',
 ]
 
+document.addEventListener("DOMContentLoaded", () => {
+    let test = document.createElement("ul", { id: "pruebaid" });
+    let mainContainer = document.getElementById("main_container");
+    mainContainer.appendChild(test);
+    to_do.forEach((element, index) => {
+        let cbh = GI.createElement("div");
+        cbh.appendChild(document.createTextNode("prueba"));
+        let cbx = GI.createElement("input");
+        cbx.type = "checkbox";
+        cbx.name = "cbx" + index;
+        cbx.value = element;
+        cbx.appendChild(cbh);
+        mainContainer.appendChild(cbx);
+    });
+});
